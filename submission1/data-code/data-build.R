db4ab3000e8c1f2c824d8841fd5eccd27be0fdfe
genevievedebell <genevieve.debell@emory.edu>
2025-01-27 16:12:21 -0500

    fourth commit

--- submission1/data-code/data-build.R
+++ submission1/data-code/data-build.R
@@ -5,6 +5,8 @@
 for (y in 2007:2015) {
   ## Basic contract/plan information
   ma.path=paste0("data/input/monthly-ma-and-pdp-enrollment-by-cpsc/CPSC_Contract_Info_2015_01.csv")
+
+
   contract.info=read_csv(ma.path,
                          skip=1,
                          col_names = c("contractid","planid","org_type","plan_type",
@@ -33,55 +35,3 @@ for (y in 2007:2015) {
     filter(id_count==1) %>%
     select(-id_count)
     
-    ## Enrollments per plan
-  ma.path=paste0("data/input/monthly-ma-and-pdp-enrollment-by-cpsc/CPSC_Enrollment_Info_2015_01.csv")
-  enroll.info=read_csv(ma.path,
-                       skip=1,
-                       col_names = c("contractid","planid","ssa","fips","state","county","enrollment"),
-                       col_types = cols(
-                       contractid = col_character(),
-                       planid = col_double(),
-                       ssa = col_double(),
-                       fips = col_double(),
-                       state = col_character(),
-                       county = col_character(),
-                       enrollment = col_double()
-                       ),na="*")
-    
-
-  ## Merge contract info with enrollment info
-  plan.data = contract.info %>%
-    left_join(enroll.info, by=c("contractid", "planid")) %>%
-    mutate(year=y)
-    
-  ## Fill in missing fips codes (by state and county)
-  plan.data = plan.data %>%
-    group_by(state, county) %>%
-    fill(fips)
-
-  ## Fill in missing plan characteristics by contract and plan id
-  plan.data = plan.data %>%
-    group_by(contractid, planid) %>%
-    fill(plan_type, partd, snp, eghp, plan_name)
-  
-  ## Fill in missing contract characteristics by contractid
-  plan.data = plan.data %>%
-    group_by(contractid) %>%
-    fill(org_type,org_name,org_marketing_name,parent_org)
-    
-  ## Collapse from monthly data to yearly
-  plan.year = plan.data %>%
-    group_by(contractid, planid, fips) %>%
-    arrange(contractid, planid, fips) %>%
-    rename(avg_enrollment=enrollment)
-  
-  write_rds(plan.year,paste0("data/output/ma_data_2015.rds"))
-}
-
-full.ma.data <- read_rds("data/output/ma_data_2007.rds")
-for (y in 2008:2015) {
-  full.ma.data <- rbind(full.ma.data,read_rds(paste0("data/output/ma_data_2015.rds")))
-}
-
-write_rds(full.ma.data,"data/output/full_ma_data.rds")
-sapply(paste0("ma_data_", 2007:2015, ".rds"), unlink)
